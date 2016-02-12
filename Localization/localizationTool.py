2ae0a0432da4ee098a06c8d91d43783353dc044b
MessuKilkain <magniez.indiana@gmail.com>
2016-02-12 13:13:29 +0100

    Remove old commented code

--- Localization/localizationTool.py
+++ Localization/localizationTool.py
@@ -6,27 +6,6 @@ from __future__ import print_function
 import sys
 import translationCheck
 
-# translationCheck.processLocalization( "./Playzer" )
-
-# keysInfos = translationCheck.getKeysInfos( "./Playzer/Languages/Base.lproj/Playzer.strings" )
-# keysInfos = sorted( keysInfos, key=lambda keyInfos: keyInfos[1] ) # Does what I want : sorting by key
-# keysInfos = sorted( keysInfos, key=lambda keyInfos: keyInfos[0] )
-# keysInfos = translationCheck.getKeysInfos( "./Playzer/Languages/Base.lproj/Localizable.strings" )
-# print()
-# for (comment,key,value) in keysInfos:
-# 	print( "c :", comment )
-# 	print( "k :", key )
-# 	print( "v :", value )
-# 	print()
-
-# >>> student_tuples = [
-#         ('john', 'A', 15),
-#         ('jane', 'B', 12),
-#         ('dave', 'B', 10),
-# ]
-# >>> sorted(student_tuples, key=lambda student: student[2])   # sort by age
-# [('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
-
 argc = len( sys.argv ) - 1
 
 # INSTRUCTION NOTE : to use easily the localization tool, copy paste the following line and replace the projectFolderPath
