682a48c7de3a10288fba50c3a9cabb1e14a0f5fd
MessuKilkain <magniez.indiana@gmail.com>
2016-05-27 16:40:27 +0200

    Refactor NSString+XMLTags Extension and add attributes to it

--- Extensions/XML/NSString+XMLTags.h
+++ Extensions/XML/NSString+XMLTags.h
@@ -11,11 +11,17 @@
 @interface NSString(XMLTags)
 
 +(instancetype)stringWithTagStart:(NSString*)tagName;
++(instancetype)stringWithTagStart:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 +(instancetype)stringWithTagEnd:(NSString*)tagName;
 +(instancetype)stringWithTagAutoClosed:(NSString*)tagName;
++(instancetype)stringWithTagAutoClosed:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 +(instancetype)stringWithTag:(NSString*)tagName withStringContent:(NSString*)content;
++(instancetype)stringWithTag:(NSString*)tagName withStringContent:(NSString*)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 +(instancetype)stringWithTag:(NSString*)tagName withIntegerContent:(NSInteger)content;
++(instancetype)stringWithTag:(NSString*)tagName withIntegerContent:(NSInteger)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 +(instancetype)stringWithTag:(NSString*)tagName withDoubleContent:(double)content;
++(instancetype)stringWithTag:(NSString*)tagName withDoubleContent:(double)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
++(instancetype)stringWithTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 
 @end
 
--- Extensions/XML/NSString+XMLTags.m
+++ Extensions/XML/NSString+XMLTags.m
@@ -8,7 +8,11 @@
 
 +(instancetype)stringWithTagStart:(NSString*)tagName
 {
-    return [self stringWithFormat:@"<%1$@>", tagName];
+    return [self stringWithTagStart:tagName withTagAttributes:nil];
+}
++(instancetype)stringWithTagStart:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    return [self stringWithFormat:@"<%1$@%2$@>", tagName, [self stringWithTagAttributes:tagAttibutes]];
 }
 +(instancetype)stringWithTagEnd:(NSString*)tagName
 {
@@ -16,19 +20,54 @@
 }
 +(instancetype)stringWithTagAutoClosed:(NSString*)tagName
 {
-    return [self stringWithFormat:@"<%1$@/>", tagName];
+    return [self stringWithTagAutoClosed:tagName withTagAttributes:nil];
+}
++(instancetype)stringWithTagAutoClosed:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    return [self stringWithFormat:@"<%1$@%2$@/>", tagName, [self stringWithTagAttributes:tagAttibutes]];
 }
 +(instancetype)stringWithTag:(NSString*)tagName withStringContent:(NSString*)content
 {
-    return [self stringWithFormat:@"<%1$@>%2$@</%1$@>", tagName, content];
+    return [self stringWithTag:tagName withStringContent:content withTagAttributes:nil];
+}
++(instancetype)stringWithTag:(NSString*)tagName withStringContent:(NSString*)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    return [self stringWithFormat:@"<%1$@%3$@>%2$@</%1$@>", tagName, content, [self stringWithTagAttributes:tagAttibutes]];
 }
 +(instancetype)stringWithTag:(NSString*)tagName withIntegerContent:(NSInteger)content
 {
-    return [self stringWithFormat:@"<%1$@>%2$ld</%1$@>", tagName, (long)content];
+    return [self stringWithTag:tagName withIntegerContent:content withTagAttributes:nil];
+}
++(instancetype)stringWithTag:(NSString*)tagName withIntegerContent:(NSInteger)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    return [self stringWithTag:tagName withStringContent:[NSString stringWithFormat:@"%ld",(long)content] withTagAttributes:tagAttibutes];
 }
 +(instancetype)stringWithTag:(NSString*)tagName withDoubleContent:(double)content
 {
-    return [self stringWithFormat:@"<%1$@>%2$lf</%1$@>", tagName, content];
+    return [self stringWithTag:tagName withDoubleContent:content withTagAttributes:nil];
+}
++(instancetype)stringWithTag:(NSString*)tagName withDoubleContent:(double)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    return [self stringWithTag:tagName withStringContent:[NSString stringWithFormat:@"%lf",content] withTagAttributes:tagAttibutes];
+}
++(instancetype)stringWithTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    NSMutableString* attributesString = [NSMutableString string];
+    if(
+       tagAttibutes != nil
+       && [tagAttibutes count] > 0
+       )
+    {
+        [tagAttibutes
+         enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSString * _Nonnull obj, BOOL * _Nonnull stop) {
+             [attributesString appendFormat:@" %@=\"%@\"",key,[obj stringByReplacingOccurrencesOfString:@"\"" withString:@"&quot;"]];
+         }];
+    }
+    if( [attributesString length] > 0 )
+    {
+        [attributesString appendString:@" "];
+    }
+    return attributesString;
 }
 
 @end
