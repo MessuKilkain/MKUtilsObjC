271f668d85c272f2310b2f14dc99ca92280e2a82
MessuKilkain <magniez.indiana@gmail.com>
2016-05-27 16:51:18 +0200

    Refactor NSMutableString+XMLTags Extension and add attributes to it

--- Extensions/XML/NSMutableString+XMLTags.h
+++ Extensions/XML/NSMutableString+XMLTags.h
@@ -10,11 +10,16 @@
 @interface NSMutableString(XMLTags)
 
 -(void)appendTagStart:(NSString*)tagName;
+-(void)appendTagStart:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 -(void)appendTagEnd:(NSString*)tagName;
 -(void)appendTagAutoClosed:(NSString*)tagName;
+-(void)appendTagAutoClosed:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 -(void)appendTag:(NSString*)tagName withStringContent:(NSString*)content;
+-(void)appendTag:(NSString*)tagName withStringContent:(NSString*)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 -(void)appendTag:(NSString*)tagName withIntegerContent:(NSInteger)content;
+-(void)appendTag:(NSString*)tagName withIntegerContent:(NSInteger)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 -(void)appendTag:(NSString*)tagName withDoubleContent:(double)content;
+-(void)appendTag:(NSString*)tagName withDoubleContent:(double)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes;
 
 @end
 
--- Extensions/XML/NSMutableString+XMLTags.m
+++ Extensions/XML/NSMutableString+XMLTags.m
@@ -8,27 +8,47 @@
 
 -(void)appendTagStart:(NSString*)tagName
 {
-    [self appendFormat:@"<%1$@>", tagName];
+    [self appendTagStart:tagName withTagAttributes:nil];
+}
+-(void)appendTagStart:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    [self appendString:[NSString stringWithTagStart:tagName withTagAttributes:tagAttibutes]];
 }
 -(void)appendTagEnd:(NSString*)tagName
 {
-    [self appendFormat:@"</%1$@>", tagName];
+    [self appendString:[NSString stringWithTagEnd:tagName]];
 }
 -(void)appendTagAutoClosed:(NSString*)tagName
 {
-    [self appendFormat:@"<%1$@/>", tagName];
+    [self appendTagAutoClosed:tagName withTagAttributes:nil];
+}
+-(void)appendTagAutoClosed:(NSString*)tagName withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    [self appendString:[NSString stringWithTagAutoClosed:tagName withTagAttributes:tagAttibutes]];
 }
 -(void)appendTag:(NSString*)tagName withStringContent:(NSString*)content
 {
-    [self appendFormat:@"<%1$@>%2$@</%1$@>", tagName, content];
+    [self appendTag:tagName withStringContent:content withTagAttributes:nil];
+}
+-(void)appendTag:(NSString*)tagName withStringContent:(NSString*)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    [self appendString:[NSString stringWithTag:tagName withStringContent:content withTagAttributes:tagAttibutes]];
 }
 -(void)appendTag:(NSString*)tagName withIntegerContent:(NSInteger)content
 {
-    [self appendFormat:@"<%1$@>%2$ld</%1$@>", tagName, (long)content];
+    [self appendTag:tagName withIntegerContent:content withTagAttributes:nil];
+}
+-(void)appendTag:(NSString*)tagName withIntegerContent:(NSInteger)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    [self appendString:[NSString stringWithTag:tagName withIntegerContent:content withTagAttributes:tagAttibutes]];
 }
 -(void)appendTag:(NSString*)tagName withDoubleContent:(double)content
 {
-    [self appendFormat:@"<%1$@>%2$lf</%1$@>", tagName, content];
+    [self appendTag:tagName withDoubleContent:content withTagAttributes:nil];
+}
+-(void)appendTag:(NSString*)tagName withDoubleContent:(double)content withTagAttributes:(NSDictionary<NSString*,NSString*>*)tagAttibutes
+{
+    [self appendString:[NSString stringWithTag:tagName withDoubleContent:content withTagAttributes:tagAttibutes]];
 }
 
 @end
